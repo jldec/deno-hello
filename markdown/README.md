548234a17926830c2b025a9ef296dfe33114e14e
Jurgen Leschner <jldec@ciaosoft.com>
2021-03-03 20:44:06 +0000

    publish on Cloudflare Pages

---
+++ markdown/README.md
@@ -0,0 +1,14 @@
+---- / ----
+name: README
+
+## Deno script hosting
+
+Scripts for [github.com/jldec/deno-hello](https://github.com/jldec/deno-hello/) hosted on [Cloudflare Pages](https://pages.cloudflare.com).
+
+For details see [jldec.me/getting-started-with-deno](https://jldec.me/getting-started-with-deno) and [jldec.me/first-steps-using-cloudflare-pages](https://jldec.me/first-steps-using-cloudflare-pages).
+
+### scripts
+
+- [hello.js](hello.js) - Hello world.
+
+- [scan.js](scan.js) - Basic crawler to check internal links.
---
+++ pub-config.js
@@ -0,0 +1,30 @@
+// pub-config for the deno-hello script hosting site at jldec.de
+// uses pub-theme-doc
+
+var opts = module.exports = {
+
+  appUrl: 'https://jldec.de',
+  docTitle: 'deno-hello',
+  github: 'https://github.com/jldec/deno-hello',
+  copyright: 'Copyright (c) 2021 Jürgen Leschner - github.com/jldec - MIT license',
+
+  pkgs: [
+    'pub-theme-doc',
+    'pub-pkg-seo'
+  ],
+
+  sources: [
+  { path: './markdown',
+    writable: true }
+  ],
+
+  outputs: [
+  { path: './out',
+    relPaths: true }
+  ],
+
+  staticPaths: [
+    { path:'.', depth:1, glob:'**/*.{js,ts}'},
+  ],
+
+};

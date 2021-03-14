ffb0047dfc3851a10a1c99be836e935fdeeca216
Jurgen Leschner <jldec@ciaosoft.com>
2021-03-14 10:16:05 -0400

    remove scan-windows.exe

--- compile.sh
+++ compile.sh
@@ -1,3 +1,2 @@
-deno --unstable compile --allow-net --lite --target x86_64-pc-windows-msvc --output scan-windows.exe scan.js
 deno --unstable compile --allow-net --lite --target x86_64-unknown-linux-gnu --output scan-linux scan.js
 deno --unstable compile --allow-net --lite --target x86_64-apple-darwin --output scan-macos scan.js
